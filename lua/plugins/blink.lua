d259a3125089b02c0531ec17aead32467178ff9e
nguyn21012003 <nguyendeptrai980@gmail.com>
2026-01-24 13:55:28 +0700

    update dot

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,143 +1,150 @@
 local icons = {
-  Break = "󰙧",
-  Call = "󰃷",
-  Case = "󰬶",
-  Class = "󰠱",
-  Color = "󰏘",
-  Constant = "󰏿",
-  Constructor = "",
-  Continue = "󰞘",
-  Declaration = "󰙠",
-  Delete = "󱟁",
-  Enum = "",
-  EnumMember = "",
-  Event = "",
-  Field = "󰇽",
-  File = "󰈙",
-  Folder = "󰉋",
-  Fragment = "",
-  Function = "󰊕",
-  Implementation = "",
-  Interface = "",
-  Keyword = "󰌋",
-  List = "󰅪",
-  Loop = "󰑖",
-  Method = "󰆧",
-  Module = "",
-  Namespace = "󰌗",
-  Operator = "󰆕",
-  Package = "",
-  Property = "󰜢",
-  Reference = "",
-  Regex = "",
-  Snippet = "",
-  Statement = "󰅩",
-  Struct = "",
-  Switch = "",
-  Text = "󰉿",
-  TypeParameter = "󰅲",
-  Undefined = "",
-  Unit = "",
-  Value = "󰎠",
-  Variable = "",
-  Macro = "",
-  Parameter = "",
-  StaticMethod = "",
-  Terminal = "",
-  TypeAlias = "",
+	Break = "󰙧",
+	Call = "󰃷",
+	Case = "󰬶",
+	Class = "󰠱",
+	Color = "󰏘",
+	Constant = "󰏿",
+	Constructor = "",
+	Continue = "󰞘",
+	Declaration = "󰙠",
+	Delete = "󱟁",
+	Enum = "",
+	EnumMember = "",
+	Event = "",
+	Field = "󰇽",
+	File = "󰈙",
+	Folder = "󰉋",
+	Fragment = "",
+	Function = "󰊕",
+	Implementation = "",
+	Interface = "",
+	Keyword = "󰌋",
+	List = "󰅪",
+	Loop = "󰑖",
+	Method = "󰆧",
+	Module = "",
+	Namespace = "󰌗",
+	Operator = "󰆕",
+	Package = "",
+	Property = "󰜢",
+	Reference = "",
+	Regex = "",
+	Snippet = "",
+	Statement = "󰅩",
+	Struct = "",
+	Switch = "",
+	Text = "󰉿",
+	TypeParameter = "󰅲",
+	Undefined = "",
+	Unit = "",
+	Value = "󰎠",
+	Variable = "",
+	Macro = "",
+	Parameter = "",
+	StaticMethod = "",
+	Terminal = "",
+	TypeAlias = "",
 }
 
 return {
-  {
-    "saghen/blink.cmp",
-    event = { "InsertEnter", "CmdlineEnter" },
-    dependencies = {
-      "rafamadriz/friendly-snippets",
-      "roobert/tailwindcss-colorizer-cmp.nvim",
-      "erooke/blink-cmp-latex",
-      {
-        "saghen/blink.compat",
-        optional = true, -- make optional so it's only enabled if any extras need it
-        opts = {},
-        version = not vim.g.lazyvim_blink_main and "*",
-      },
-    },
-    opts = {
-      snippets = {
-        preset = "luasnip",
-      },
-      sources = {
-        default = { "latex" },
-        providers = {
-          latex = {
-            name = "Latex",
-            module = "blink-cmp-latex",
-            opts = {
-              insert_command = true,
-            },
-          },
-        },
-      },
-      completion = {
-        ghost_text = {
-          enabled = false,
-        },
-        menu = {
-          auto_show = true,
-          scrollbar = false,
-          border = "rounded",
-          draw = {
-            components = {
-              kind_icon = {
-                text = function(ctx)
-                  if require("blink.cmp.sources.lsp.hacks.tailwind").get_hex_color(ctx.item) then
-                    return " "
-                  end
-                  return icons[ctx.kind] .. ctx.icon_gap
-                end,
-              },
-              kind = {
-                text = function(ctx)
-                  return "" .. ctx.kind .. ""
-                end,
-              },
-              source = {
-                width = { max = 30 },
-                text = function(ctx)
-                  return ({
-                    LSP = "[LSP]",
-                    Buffer = "[BUF]",
-                    Snippets = "[SNIP]",
-                    Latex_symbols = "[LTEX]",
-                    Path = "[PATH]",
-                    Spell = "[SPELL]",
-                    Cmdline = "[CMD]",
-                    Copilot = "[ ]",
-                  })[ctx.source_name]
-                end,
-                highlight = "BlinkCmpDoc",
-              },
-            },
-            columns = { { "label", "label_description", gap = 1 }, { "kind_icon", "kind", gap = 2 }, { "source" } },
-          },
-        },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 100,
-          update_delay_ms = 50,
-          window = { border = "rounded" },
-        },
-      },
-      keymap = {
-        preset = "default",
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<CR>"] = { "accept", "fallback" },
-        ["<Up>"] = { "select_prev", "fallback" },
-        ["<Down>"] = { "select_next", "fallback" },
-        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
-      },
-    },
-  },
+	{
+		"saghen/blink.cmp",
+		event = { "InsertEnter", "CmdlineEnter" },
+		dependencies = {
+			"rafamadriz/friendly-snippets",
+			"roobert/tailwindcss-colorizer-cmp.nvim",
+			"erooke/blink-cmp-latex",
+			{
+				"saghen/blink.compat",
+				optional = true, -- make optional so it's only enabled if any extras need it
+				opts = {},
+				version = not vim.g.lazyvim_blink_main and "*",
+			},
+		},
+		opts = {
+			fuzzy = {
+				implementation = "lua",
+			},
+			snippets = {
+				preset = "luasnip",
+			},
+			sources = {
+				default = { "latex" },
+				providers = {
+					latex = {
+						name = "Latex",
+						module = "blink-cmp-latex",
+						opts = {
+							insert_command = true,
+						},
+					},
+				},
+			},
+			completion = {
+				ghost_text = {
+					enabled = false,
+				},
+				menu = {
+					auto_show = true,
+					scrollbar = false,
+					border = "rounded",
+					draw = {
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									if require("blink.cmp.sources.lsp.hacks.tailwind").get_hex_color(ctx.item) then
+										return " "
+									end
+									return icons[ctx.kind] .. ctx.icon_gap
+								end,
+							},
+							kind = {
+								text = function(ctx)
+									return "" .. ctx.kind .. ""
+								end,
+							},
+							source = {
+								width = { max = 30 },
+								text = function(ctx)
+									return ({
+										LSP = "[LSP]",
+										Buffer = "[BUF]",
+										Snippets = "[SNIP]",
+										Latex_symbols = "[LTEX]",
+										Path = "[PATH]",
+										Spell = "[SPELL]",
+										Cmdline = "[CMD]",
+										Copilot = "[ ]",
+									})[ctx.source_name]
+								end,
+								highlight = "BlinkCmpDoc",
+							},
+						},
+						columns = {
+							{ "label", "label_description", gap = 1 },
+							{ "kind_icon", "kind", gap = 2 },
+							{ "source" },
+						},
+					},
+				},
+				documentation = {
+					auto_show = true,
+					auto_show_delay_ms = 100,
+					update_delay_ms = 50,
+					window = { border = "rounded" },
+				},
+			},
+			keymap = {
+				preset = "default",
+				["<Tab>"] = { "select_next", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
+				["<CR>"] = { "accept", "fallback" },
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
+			},
+		},
+	},
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,209 +1,209 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    lazy = true,
-    opts = {
-      style = "night",
-      transparent = true,
-      styles = {
-        comment = { italic = true },
-        floats = "transparent",
-        sidebars = "transparent",
-      },
-      on_highlights = function(hl, c)
-        hl.LspKindVariable = { fg = c.orange, italic = true }
-        hl.LspKindText = { fg = c.green, italic = false }
-      end,
-    },
-  },
-  {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "rcarriga/nvim-notify",
-      "MunifTanjim/nui.nvim",
-    },
-    opts = {
-      notify = {
-        enabled = true,
-        view = "notify",
-        opts = { timeout = 1000 },
-      },
-      cmdline = {
-        enabled = true,
-        view = "cmdline_popup",
-        format = {
-          cmdline = {
-            -- icon = ":",
-          },
-        },
-      },
-      presets = {
-        command_palette = false,
-        bottom_search = false,
-      },
-      messages = {
-        enabled = true,
-        timeout = 1,
-      },
-      popupmenu = {
-        enabled = true,
-        backend = "cmp",
-      },
-      lsp = {
-        progress = { enabled = true },
-        signature = { enabled = false },
-        hover = { enabled = true, opts = { border = "rounded" } },
-        presets = {
-          lsp_doc_border = false,
-        },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-        },
-      },
-    },
-  },
-  {
-    "akinsho/bufferline.nvim",
-    opts = {
-      options = {
-        show_buffer_close_icons = false,
-        always_show_bufferline = true,
-        numbers = "ordinal",
-      },
-    },
-  },
-  {
-    "sphamba/smear-cursor.nvim",
-    enabled = false,
-    opts = {},
-  },
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    enabled = true,
-    dependencies = {
-      "SmiteshP/nvim-navic",
-      "lewis6991/gitsigns.nvim",
-      "arkav/lualine-lsp-progress",
-    },
-    -- opts = function()
-    --   local navic = require("nvim-navic")
-    --   local git_blame = require("gitsigns")
-    --   local colors = {
-    --     yellow = "#ECBE7B",
-    --     cyan = "#008080",
-    --     darkblue = "#081633",
-    --     green = "#98be65",
-    --     orange = "#FF8800",
-    --     violet = "#1a1b26",
-    --     magenta = "#c678dd",
-    --     blue = "#51afef",
-    --     red = "#ec5f67",
-    --   }
-    --
-    --   local progress = function()
-    --     local current_line = vim.fn.line(".")
-    --     local total_lines = vim.fn.line("$")
-    --     local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
-    --     local line_ratio = current_line / total_lines
-    --     local index = math.ceil(line_ratio * #chars)
-    --     return chars[index]
-    --   end
-    --
-    --   local config = {
-    --     options = {
-    --       disabled_filetypes = {
-    --         statusline = { "NvimTree", "packer", "alpha" },
-    --         winbar = { "help", "qf", "Trouble", "SearchBoxPrompt", "tex" },
-    --       },
-    --       theme = "tokyonight-moon",
-    --       icons_enabled = true,
-    --       component_separators = { left = "", right = "" },
-    --       -- section_separators = { left = "", right = "" },
-    --       section_separators = { left = " ", right = " " },
-    --     },
-    --     inactive_winbar = {},
-    --     inactive_sections = {
-    --       lualine_a = {},
-    --       lualine_b = {},
-    --       lualine_c = { "filename" },
-    --       lualine_x = { "location" },
-    --       lualine_y = {},
-    --       lualine_z = {},
-    --     },
-    --     winbar = {
-    --       -- lualine_a = {
-    --       --   { "filename", path = 1 },
-    --       --   {
-    --       --     function()
-    --       --       return navic.get_location()
-    --       --     end,
-    --       --     cond = function()
-    --       --       return navic.is_available()
-    --       --     end,
-    --       --   },
-    --       -- },
-    --       -- lualine_y = {},
-    --     },
-    --     sections = {
-    --       lualine_c = {
-    --         {
-    --           "selectioncount",
-    --         },
-    --       },
-    --       lualine_x = {
-    --         -- {
-    --         --   "datetime",
-    --         --   style = "%a/%d/%m %H:%M",
-    --         -- },
-    --         {
-    --           "location",
-    --         },
-    --       },
-    --       lualine_y = {
-    --         {
-    --           "filetype",
-    --           colored = true,
-    --           icon_only = true,
-    --         },
-    --       },
-    --       lualine_z = {
-    --         -- { progress, color = { fg = colors.yellow, gui = "bold" } },
-    --         {
-    --           "datetime",
-    --           style = "%a/%d/%m %H:%M",
-    --         },
-    --       },
-    --     },
-    --   }
-    --   table.insert(config.sections.lualine_x, {
-    --     "lsp_progress",
-    --     display_components = { "lsp_client_name", "spinner", { "title", "percentage", "message" } },
-    --     colors = {
-    --       percentage = colors.cyan,
-    --       title = colors.cyan,
-    --       message = colors.cyan,
-    --       spinner = colors.cyan,
-    --       lsp_client_name = colors.magenta,
-    --       use = true,
-    --     },
-    --     separators = {
-    --       component = " ",
-    --       progress = " | ",
-    --       message = { pre = "(", post = ")" },
-    --       percentage = { pre = "", post = "%% " },
-    --       title = { pre = "", post = ": " },
-    --       lsp_client_name = { pre = "[", post = "]" },
-    --       spinner = { pre = "", post = "" },
-    --     },
-    --     timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
-    --     spinner_symbols = { "🌑 ", "🌒 ", "🌓 ", "🌔 ", "🌕 ", "🌖 ", "🌗 ", "🌘 " },
-    --   })
-    --
-    --   return config
-    -- end,
-  },
+	{
+		"folke/tokyonight.nvim",
+		lazy = true,
+		opts = {
+			style = "night",
+			transparent = true,
+			styles = {
+				comment = { italic = true },
+				floats = "transparent",
+				sidebars = "transparent",
+			},
+			on_highlights = function(hl, c)
+				hl.LspKindVariable = { fg = c.orange, italic = true }
+				hl.LspKindText = { fg = c.green, italic = false }
+			end,
+		},
+	},
+	{
+		"folke/noice.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"rcarriga/nvim-notify",
+			"MunifTanjim/nui.nvim",
+		},
+		opts = {
+			notify = {
+				enabled = true,
+				view = "notify",
+				opts = { timeout = 1000 },
+			},
+			cmdline = {
+				enabled = true,
+				view = "cmdline_popup",
+				format = {
+					cmdline = {
+						-- icon = ":",
+					},
+				},
+			},
+			presets = {
+				command_palette = false,
+				bottom_search = false,
+			},
+			messages = {
+				enabled = true,
+				timeout = 1,
+			},
+			popupmenu = {
+				enabled = true,
+				backend = "cmp",
+			},
+			lsp = {
+				progress = { enabled = true },
+				signature = { enabled = false },
+				hover = { enabled = true, opts = { border = "rounded" } },
+				presets = {
+					lsp_doc_border = false,
+				},
+				override = {
+					["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+					["vim.lsp.util.stylize_markdown"] = true,
+					["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+				},
+			},
+		},
+	},
+	{
+		"akinsho/bufferline.nvim",
+		opts = {
+			options = {
+				show_buffer_close_icons = false,
+				always_show_bufferline = true,
+				numbers = "ordinal",
+			},
+		},
+	},
+	{
+		"sphamba/smear-cursor.nvim",
+		enabled = false,
+		opts = {},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		enabled = true,
+		dependencies = {
+			"SmiteshP/nvim-navic",
+			"lewis6991/gitsigns.nvim",
+			"arkav/lualine-lsp-progress",
+		},
+		opts = function()
+			local navic = require("nvim-navic")
+			local git_blame = require("gitsigns")
+			local colors = {
+				yellow = "#ECBE7B",
+				cyan = "#008080",
+				darkblue = "#081633",
+				green = "#98be65",
+				orange = "#FF8800",
+				violet = "#1a1b26",
+				magenta = "#c678dd",
+				blue = "#51afef",
+				red = "#ec5f67",
+			}
+
+			local progress = function()
+				local current_line = vim.fn.line(".")
+				local total_lines = vim.fn.line("$")
+				local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+				local line_ratio = current_line / total_lines
+				local index = math.ceil(line_ratio * #chars)
+				return chars[index]
+			end
+
+			local config = {
+				options = {
+					disabled_filetypes = {
+						statusline = { "NvimTree", "packer", "alpha" },
+						winbar = { "help", "qf", "Trouble", "SearchBoxPrompt", "tex" },
+					},
+					theme = "tokyonight-moon",
+					icons_enabled = true,
+					component_separators = { left = "", right = "" },
+					-- section_separators = { left = "", right = "" },
+					section_separators = { left = " ", right = " " },
+				},
+				inactive_winbar = {},
+				inactive_sections = {
+					lualine_a = {},
+					lualine_b = {},
+					lualine_c = { "filename" },
+					lualine_x = { "location" },
+					lualine_y = {},
+					lualine_z = {},
+				},
+				winbar = {
+					-- lualine_a = {
+					--   { "filename", path = 1 },
+					--   {
+					--     function()
+					--       return navic.get_location()
+					--     end,
+					--     cond = function()
+					--       return navic.is_available()
+					--     end,
+					--   },
+					-- },
+					-- lualine_y = {},
+				},
+				sections = {
+					lualine_c = {
+						{
+							"selectioncount",
+						},
+					},
+					lualine_x = {
+						-- {
+						--   "datetime",
+						--   style = "%a/%d/%m %H:%M",
+						-- },
+						{
+							"location",
+						},
+					},
+					lualine_y = {
+						{
+							"filetype",
+							colored = true,
+							icon_only = true,
+						},
+					},
+					lualine_z = {
+						-- { progress, color = { fg = colors.yellow, gui = "bold" } },
+						{
+							"datetime",
+							style = "%a/%d/%m %H:%M",
+						},
+					},
+				},
+			}
+			table.insert(config.sections.lualine_x, {
+				"lsp_progress",
+				display_components = { "lsp_client_name", "spinner", { "title", "percentage", "message" } },
+				colors = {
+					percentage = colors.cyan,
+					title = colors.cyan,
+					message = colors.cyan,
+					spinner = colors.cyan,
+					lsp_client_name = colors.magenta,
+					use = true,
+				},
+				separators = {
+					component = " ",
+					progress = " | ",
+					message = { pre = "(", post = ")" },
+					percentage = { pre = "", post = "%% " },
+					title = { pre = "", post = ": " },
+					lsp_client_name = { pre = "[", post = "]" },
+					spinner = { pre = "", post = "" },
+				},
+				timer = { progress_enddelay = 500, spinner = 1000, lsp_client_name_enddelay = 1000 },
+				spinner_symbols = { "🌑 ", "🌒 ", "🌓 ", "🌔 ", "🌕 ", "🌖 ", "🌗 ", "🌘 " },
+			})
+
+			return config
+		end,
+	},
 }
