003750bb9809bfbcd0d9ed6d63e0a9f264b5fe86
nguyn21012003 <nguyendeptrai980@gmail.com>
2026-02-26 22:09:19 +0700

    update neovim

--- lua/plugins/blame.lua
+++ lua/plugins/blame.lua
@@ -1,10 +1,10 @@
 return {
-  "f-person/git-blame.nvim",
-  -- event = "VeryLazy",
-  cmd = { "GitBlameEnable", "GitBlameDisable" },
-  opts = {
-    message_template = "<author> • <date> • <summary>",
-    date_format = "%d/%m/%Y",
-    virtual_text_column = 0,
-  },
+	"f-person/git-blame.nvim",
+	-- event = "VeryLazy",
+	cmd = { "GitBlameEnable", "GitBlameDisable" },
+	opts = {
+		message_template = "<author> • <date> • <summary>",
+		date_format = "%d/%m/%Y",
+		virtual_text_column = 1,
+	},
 }
